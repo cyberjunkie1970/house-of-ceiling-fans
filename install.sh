3a21d195abd5b5b812f84ba41d14bdfc37104cb0
cyberjunkie1970 <87285127+cyberjunkie1970@users.noreply.github.com>
2021-07-14 13:48:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ cp SMBbrowse.py /usr/bin
 mkdir -p /usr/share/pixmaps/SMBbrowse
 cp *.png /usr/share/pixmaps/SMBbrowse
 cp SMBbrowse.desktop /usr/share/applications
+mkdir -p /usr/share/licenses/SMBbrowse
+cp LICENSE /usr/share/licenses/SMBbrowse
